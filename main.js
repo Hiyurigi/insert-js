4dadf8d3efeec2d10ad5515a20dd9c57189248f4
Kaffu theine <37732276+Hiyurigi@users.noreply.github.com>
2023-02-05 21:58:00 +0700

    Update main.js

--- main.js
+++ main.js
@@ -9,3 +9,9 @@ function insert(src, init){
   document.body.appendChild(script_el(src));
   document.body.appendChild(script_el({body: init}));
 }
+function eruda(){
+  insert("//cdn.jsdelivr.net/npm/eruda", "eruda.init();");
+}
+function vConsole(){
+  insert("//unpkg.com/vconsole@latest/dist/vconsole.min.js", "new window.VConsole();");
+}
